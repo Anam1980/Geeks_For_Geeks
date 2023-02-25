9f9ad97c411e5c226e15ffa3a6a18958a0bda7fa
Anam Sheikh <113501770+Anam1980@users.noreply.github.com>
2023-02-25 17:43:57 +0530

    Update Detect cycle in a undirected graph.java

--- Graph/Detect cycle in a undirected graph.java
+++ Graph/Detect cycle in a undirected graph.java
@@ -1,36 +1,32 @@
 
-/*Complete the function below*/
-
 class Solution {
     
-    public boolean dfs(int curr, boolean[]mark,  boolean[] path, ArrayList<ArrayList<Integer>> adj){
-        //mark
-        mark[curr]=true;
-        path[curr]=true;
+    boolean dfs(int node, int parent, boolean[]mark, ArrayList<ArrayList<Integer>> adj){
+        mark[node]=true;
         
-        for(int nbr : adj.get(curr)){
-            
-            if(mark[nbr] && path[nbr])return true;
-            
-            if(!mark[nbr]){
-                boolean check=dfs(nbr, mark, path, adj);
-                
+        for(int nbr : adj.get(node)){
+            if(mark[nbr]==false){
+                boolean check=dfs(nbr, node, mark, adj);
                 if(check)return true;
             }
+            else if(mark[nbr]==true){
+                if(parent != nbr)return true;
+            }
         }
-        path[curr]=false;
         return false;
     }
-    // Function to detect cycle in a directed graph.
-    public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
+    // Function to detect cycle in an undirected graph.
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+    
+    
       boolean[]mark=new boolean[V];
-      boolean[]path=new boolean[V];
       
       for(int i=0; i<V; i++){
-          if(dfs(i, mark, path, adj))return true;
+          if(mark[i]==true)continue;
+          
+          if(dfs(i, -1, mark, adj))return true;
       }
       
       return false;
-      
     }
 }
